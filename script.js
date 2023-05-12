275f01278e2fa54576ef974f94dcf5301bbd5eb8
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-05-12 07:31:26 +0600

    toggle btn is added

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const settingMenu = document.querySelector(".settings-menu");
+const darkBtn = document.querySelector(".dark-btn");
 
 function toggleSettingsMenu() {
   settingMenu.classList.toggle("settings-menu-show");
 }
+
+darkBtn.onclick = function () {
+  darkBtn.classList.toggle("dark-btn-on");
+};
